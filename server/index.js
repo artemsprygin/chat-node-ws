2db5e1069d3c1c8b3db7531f0bceb57a9c37c38c
artemsprygin <artemsprygin@me.com>
2018-07-25 14:17:00 +0300

    add final project

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const server = require('ws').Server;
+const s = new server({port: 5001});
+
+s.on('connection',(ws) => {
+  ws.on('message',(message) => {
+
+    message = JSON.parse(message);
+
+    if (message.type == 'name'){
+        ws.personName = message.data
+        return;
+    }
+
+    console.log('Recieved ' + message);
+    // if (message == 'hello') {
+    //   ws.send('hi from server');
+    // }
+    s.clients.forEach(function e(client) {
+      if (client != ws)
+      client.send(JSON.stringify({
+        name: ws.personName,
+        data: message.data
+      }));
+    });
+    // ws.send('From server: ' + message);
+  });
+  ws.on('close',() => {
+    console.log('I lost a client');
+  });
+  console.log('one more client connected');
+});
